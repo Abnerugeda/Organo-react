eace0b5617f788cd3bd7cb03d49600d8656ab74f
Abner Ugeda <devabnerugeda@gmail.com>
2023-05-24 13:58:47 -0300

    Input color sendo criado

--- src/App.js
+++ src/App.js
@@ -52,17 +52,23 @@ function App() {
     setColaboradores([...colaboradores, colaborador])
   }
 
+  function deletarColaborador(){
+    console.log("Deletando colaborador")
+  }
   return (
     <div className="App">
       <Banner/>
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      {times.map(time => <Time 
+      {times.map(time =>
+       <Time 
         key={time.nome}
         nome={time.nome} 
         corPrimaria={time.corPrimaria} 
         corSecundaria={time.corSecundaria}
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
-      />)}
+        aoDeletar={deletarColaborador}
+      />)
+      }
 
     </div>
   );
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -7,9 +7,19 @@ const Time = (props) => {
 
     return (
         props.colaboradores.length >0 && <section className='time' style={css}>
+            <input type='color' className='input-color'/>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map(colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+                {props.colaboradores.map(colaborador => {
+                    return <Colaborador 
+                    corDeFundo={props.corPrimaria} 
+                    key={colaborador.nome} 
+                    nome={colaborador.nome} 
+                    cargo={colaborador.cargo} 
+                    imagem={colaborador.imagem}
+                    aoDeletar={props.aoDeletar}
+                    />
+                })}
             </div>
         </section>
     )
