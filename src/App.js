5808829f26402fc5123c1138f4aebe548c359e7c
Abner Ugeda <devabnerugeda@gmail.com>
2023-05-25 13:14:03 -0300

    Cores reativas

--- src/App.js
+++ src/App.js
@@ -9,38 +9,31 @@ function App() {
   const [times, setTimes] = useState([
     {
       nome: 'Programação',
-      corPrimaria: '#57c278',
-      corSecundaria: '#D9F7E9'
+      cor: '#D9F7E9'
     },
     {
       nome: 'Front End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      cor: '#E8F8FF'
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      cor: '#F0F8E2'
     },
     {
       nome: 'Devops',
-      corPrimaria: '#ED6869',
-      corSecundaria: '#FDE7E8'
+      cor: '#FDE7E8'
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#D86E8F',
-      corSecundaria: '#FAE9FS'
+      cor: '#FAE9FS'
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FF8A05',
-      corSecundaria: '#FFF5D9'
+      cor: '#FFF5D9'
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#FFEEDF'
     },
   
   ])
@@ -59,7 +52,7 @@ function App() {
   function mudarCorDoTime(cor, nome){
     setTimes(times.map(time => {
       if(time.nome === nome){
-        time.corPrimaria = cor;
+        time.cor = cor;
       }
       return time;
     }))
@@ -73,8 +66,8 @@ function App() {
         mudarCor={mudarCorDoTime}
         key={time.nome}
         nome={time.nome} 
-        corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria}
+        corPrimaria={time.cor} 
+        cor={time.cor}
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
         aoDeletar={deletarColaborador}
       />)
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,18 +1,19 @@
+import hexToRgba from 'hex-to-rgba'
 import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
 
-    const css = {backgroundColor: props.corSecundaria}
+    const css = {backgroundColor: hexToRgba(props.cor, '0.6')}
 
     return (
         props.colaboradores.length >0 && <section className='time' style={css}>
-            <input onChange={evento => props.mudarCor(evento.target.value, props.nome)}type='color' className='input-color'/>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <input onChange={evento => props.mudarCor(evento.target.value, props.nome)} type='color' className='input-color'/>
+            <h3 style={{borderColor: props.cor}}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map(colaborador => {
                     return <Colaborador 
-                    corDeFundo={props.corPrimaria} 
+                    corDeFundo={props.cor} 
                     key={colaborador.nome} 
                     nome={colaborador.nome} 
                     cargo={colaborador.cargo} 
